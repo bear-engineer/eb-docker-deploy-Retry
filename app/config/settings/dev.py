47ccb93745bdcc086dee5e7ad8e8f78b286496d4
Sehyeon Youn <d.sehyeon@gmail.com>
2018-07-15 03:54:22 +0900

    storage s3 ling

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -1,10 +1,17 @@
 from .base import *
+# 개발용 Setting
+secret = json.load(open(os.path.join(SECRET_DIR, 'secrets.json')))
 
 DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1'
+]
+
+# django-storages
+INSTALLED_APPS += [
+    'storages',
 ]
 
 WSGI_APPLICATION = 'config.wsgi.dev.application'
@@ -15,10 +22,18 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+DEFAULT_FILE_STORAGE = 'config.storage.S3DefaultStorage'
+STATICFILES_STORAGE = 'config.storage.S3StaticStorage'
+
+AWS_ACCESS_KEY_ID = secret['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = secret['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = secret['AWS_STORAGE_BUCKET_NAME_DEV']
+AWS_DEFAULT_ACL = secret['AWS_DEFAULT_ACL']
+AWS_S3_REGION_NAME = secret['AWS_S3_REGION_NAME']
+AWS_S3_SIGNATURE_VERSION = secret['AWS_S3_SIGNATURE_VERSION']
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-secret = json.load(open(os.path.join(SECRET_DIR, 'secrets.json')))
 
 DATABASES = secret['DATABASE_DEV']
