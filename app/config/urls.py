62403731b5a3250619f82ad48ee86966f055d100
Sehyeon Youn <d.sehyeon@gmail.com>
2018-07-14 16:14:36 +0900

    index link create

--- app/config/urls.py
+++ app/config/urls.py
@@ -17,9 +17,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index)
 ] + static(
     prefix=settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT,
